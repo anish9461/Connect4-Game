c8fbe901df448083ebb0cf250a6da1a3ab6f0156
anish9461 <anish9461@gmail.com>
2019-08-08 09:41:42 -0400

    Added Infor Bar

---
+++ connect4/src/components/InfoBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const InfoBar = ({message, color}) => {
+    let style = 
+    {
+        color: color,
+        backgroundColor: 'black',
+        padding: '5px'
+    };
+    return( 
+        <p style={style}>{message}</p>
+    )
+}
+
+export default InfoBar;
